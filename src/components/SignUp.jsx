98a890dd3b26d80106dbfb5b7e8d24a3228cd22b
jonathanalaksana <jonathanalaksana@gmail.com>
2022-09-27 18:31:38 -0700

    connect signup to firebase

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,64 @@
-import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, db, storage } from "../firebase";
 import "../styles/Auth.css";
 
 function SignUp() {
-  const handleSubmit = () => {};
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const user = res.user;
+
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", user.uid), {
+              uid: user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", user.uid), {});
+            navigate("/");
+          } catch (err) {
+            console.log(err);
+            setError(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
+      setError(true);
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="auth">
@@ -14,6 +70,7 @@ function SignUp() {
           id="display-name"
           name="display-name"
           placeholder="Display Name"
+          required
         />
         <input type="email" id="email" name="email" placeholder="Email" />
         <input
@@ -21,7 +78,18 @@ function SignUp() {
           id="password"
           name="password"
           placeholder="password"
+          required
         />
+        <div>
+          <p>Upload Profile Picture</p>
+          <input type="file" name="Avatar" />
+        </div>
+        {loading && "Uploading image..."}
+        {error && (
+          <span className="error">
+            Something went wrong. Try with a different email or longer password
+          </span>
+        )}
         <button type="submit">Sign Up</button>
       </form>
       <p>
