1cb4a1408b4bcf6c3c9ca6dcfb70f1142be8d61b
jonathanalaksana <jonathanalaksana@gmail.com>
2022-10-04 17:26:43 -0700

    message area ui

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { FiMoreVertical } from "react-icons/fi";
+import { IoMdAdd } from "react-icons/io";
+import "../styles/Chat.css";
+import Input from "./Input";
+import Messages from "./Messages";
 
 function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jonathan</span>
+        <div className="chatIcons">
+          <IoMdAdd />
+          <FiMoreVertical />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 }
 
 export default Chat;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BiImageAdd } from "react-icons/bi";
+import { IoMdSend } from "react-icons/io";
 
 function Input() {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type a message..." />
+      <div className="send">
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <BiImageAdd />
+        </label>
+        <div className="submit">
+          <IoMdSend />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Input;
